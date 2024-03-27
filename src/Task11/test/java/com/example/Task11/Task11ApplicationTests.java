91924a95c4681bcbd50d74cd7b12ae96feaa68ff
Admin <aj1ax@yandex.ru>
2024-03-27 20:15:21 +0300

    +Task 12

---
+++ src/Task11/test/java/com/example/Task11/Task11ApplicationTests.java
@@ -0,0 +1,13 @@
+package Task11.test.java.com.example.Task11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
